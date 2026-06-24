c3398e0049eb156c99d8f34346728234ee91108c
Dhananjay Ghosh <dhananjayghosh146@gmail.com>
2026-06-24 21:19:21 +0530

    footeradded

--- healthapp/src/App.jsx
+++ healthapp/src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import Header from './components/header/header.jsx'
+import Footer from './components/footer/footer.jsx';
 import './App.css'
 
 function App() {
 
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App
